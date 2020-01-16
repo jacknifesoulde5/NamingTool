959c70ac0cd150500da28ce76bfad86140f9e5e5
jacknifesoulde5 <jacknifesoulde5@gmail.com>
2020-01-16 23:03:04 +0900

    ツール画面ロジック修正

--- app/controllers/tool_controller.rb
+++ app/controllers/tool_controller.rb
@@ -13,46 +13,35 @@ class ToolController < ApplicationController
 
     #いずれはモデルに移した方が良いんだろうな。
     #変換後文字列生成
-    concrete_conv_word = ""
-    japanese_conv_word = ""
+    conv_array =[]
     if Word.where(id: conv_params[:concreteMethod]).present?
-      concrete_conv_word = Word.where(id: conv_params[:concreteMethod]).first["english_word"]
+      conv_array.push(Word.where(id: conv_params[:concreteMethod]).first["english_word"])
     end
     if Word.where(japanese_word: conv_params[:japanese_word]).present?
-      japanese_conv_word = Word.where(japanese_word: conv_params[:japanese_word]).first["english_word"]
+      conv_array.push(Word.where(japanese_word: conv_params[:japanese_word]).first["english_word"])
     end
-    conv_word = <<~CONV_WORD
-                #{concrete_conv_word}_#{japanese_conv_word}
-                CONV_WORD
 
-    render json: [{"conv_word": conv_word.downcase}]
+    render json: [{"conv_word": conv_array.join('_').downcase}]
   end
 
   def conv_other
-    #@word = Word.new(conv_params)
-
     #TODO:Strong Parameter的なことはしなくても良いのだろうか。
     #いずれはモデルに移した方が良いんだろうな。
     #変換後文字列生成
-    conv_word1 = ""
-    conv_word2 = ""
-    conv_word3 = ""
-    #binding.pry
+    conv_array =[]
     if Word.where(japanese_word: params[:word1]).present?
-      conv_word1 = Word.where(japanese_word: params[:word1]).first["english_word"]
+      conv_array.push(Word.where(japanese_word: params[:word1]).first["english_word"].downcase)
     end
     if Word.where(japanese_word: params[:word2]).present?
-      conv_word2 = Word.where(japanese_word: params[:word2]).first["english_word"]
+      conv_array.push(Word.where(japanese_word: params[:word2]).first["english_word"].downcase)
     end
     if Word.where(japanese_word: params[:word3]).present?
-      conv_word3 = Word.where(japanese_word: params[:word3]).first["english_word"]
+      conv_array.push(Word.where(japanese_word: params[:word3]).first["english_word"].downcase)
     end
-    conv_other = <<~CONV_WORD
-                 #{conv_word1}_#{conv_word2}_#{conv_word3}
-                 CONV_WORD
-
+    #スネークケースに変換
+    conv_other = conv_array.join('_')
     render json: [{"conv_other_camel": conv_other.camelize,
-                   "conv_other_downcase": conv_other.downcase,
+                   "conv_other_downcase": conv_other,
                    "conv_other_upcase": conv_other.upcase}]
   end
 
